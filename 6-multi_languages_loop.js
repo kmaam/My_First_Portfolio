bd9c89b59bc6c6343bdb2eb240c8c014c52d86e0
qmaam <abdihakimahmed890@gmail.com>
2025-05-26 22:37:29 +0300

    ~6. Loop to languages

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,2 +1,2 @@
-const lines=["C is fun","JavaScript is amazing","Python is cool"]
-for(const line of lines)console.log(line);
+const lines=["C is fun","JavaScript is amazing","Python is cool"];
+for (let i = 0; i < lines.length; i++) console.log(lines[i]);
